345a13600bed79f1d18f63c352608ec8ec1778bc
Joshua Shuttleworth <joshua.shuttleworth22@ycdsbk12.ca>
2021-06-16 09:54:59 -0400

    First steps to javaFX

--- src/classes/GPU.java
+++ src/classes/GPU.java
@@ -2,16 +2,17 @@ package classes;
 
 public class GPU {
     private int ranking;
-    private String name;
+    private String name, change;
     private double marPercent, aprPercent, mayPercent, junPercent, julPercent;
 
-    public GPU(int ranking, String name, double marPercent, double aprPercent, double mayPercent, double junPercent, double julPercent) {
+    public GPU(int ranking, String name, double marPercent, double aprPercent, double mayPercent, double junPercent, double julPercent, String change) {
         this.ranking = ranking;
         this.name = name;
         this.marPercent = marPercent;
         this.aprPercent = aprPercent;
         this.mayPercent = mayPercent;
-        this.junPercent = julPercent;      
+        this.junPercent = julPercent;   
+        this.change = change;   
     }
     public int getRanking() {
         return ranking;
@@ -33,5 +34,12 @@ public class GPU {
     }
     public double getJulPercent() {
         return julPercent;
+    }
+    public String getChange() {
+        return change;
+    }
+    @Override
+    public String toString() {
+        return getRanking() + " | " + getMarPercent() + " | " + getAprPercent() + " | " + getMayPercent() + " | " + getJunPercent() + " | " + getJulPercent() + " | " + getChange();
     }
 }
